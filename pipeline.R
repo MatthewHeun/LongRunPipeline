64269f15793032204f6a60a581f26a3fd0b63b94
Matthew Heun <matthew.heun@me.com>
2024-10-13 15:08:43 -0400

    Diagnose world electricity in 1920

--- pipeline.R
+++ pipeline.R
@@ -65,7 +65,12 @@ list(
     Balanced,
     PSUTLR |>
       # Fails at 1920, but 1900-1919 work.
-      dplyr::filter(Dataset == "world_electricity", Year <= 1920) |>
+      dplyr::filter(Dataset == "world_electricity",
+                    Year == 1920,
+                    LastStage == "Useful",
+                    EnergyType == "X") |>
+      # Rail 2018 fails, but 1840-2017 all work.
+      # dplyr::filter(Dataset == "rail", Year <= 2018) |>
       Recca::verify_SUT_energy_balance_with_units()
   )
 
