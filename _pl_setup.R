5128a87109d955c93728cb0476a9812e9474026c
MatthewHeun <matthew.heun@me.com>
2025-01-05 13:22:23 -0600

    New Rail file.

--- _pl_setup.R
+++ _pl_setup.R
@@ -2,6 +2,6 @@
 # Get file paths ---------------------------------------------------------------
 
 electricity_file <- file.path("data", "world_electricity_finalrevisited_v10.xlsx")
-rail_file <- file.path("data", "EnergyRecords_Rail_v12.xlsx")
-
+# rail_file <- file.path("data", "EnergyRecords_Rail_v12.xlsx")
+rail_file <- file.path("data", "EnergyRecords_Transport_extract_V1.xlsx")
 
