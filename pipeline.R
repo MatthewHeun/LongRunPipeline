3b337f4dd126569dbb7f6c1111d814be4c79f9cb
Matthew Heun <matthew.heun@me.com>
2025-01-20 11:52:42 -0500

    Working on pipeline

--- pipeline.R
+++ pipeline.R
@@ -25,7 +25,8 @@ list(
     LRData,
     # Uncomment after debugging Rail data.
     # load_long_run_data(paths = c(ElectricityFilePath, TransportFilePath))
-    load_long_run_data(paths = c(TransportFilePath))
+    load_long_run_data(paths = c(TransportFilePath)) |>
+      dplyr::filter(Year == 1800)
   ),
 
 
