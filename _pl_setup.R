f7247eb09bbcf9058ec81ecce225c9ada28ec8cc
MatthewHeun <matthew.heun@me.com>
2025-01-21 09:17:54 -0500

    Now up to V5

--- _pl_setup.R
+++ _pl_setup.R
@@ -2,5 +2,5 @@
 # Get file paths ---------------------------------------------------------------
 
 electricity_file <- file.path("data", "world_electricity_finalrevisited_v11.xlsx")
-transport_file <- file.path("data", "EnergyRecords_Transport_extract_V4.xlsx")
+transport_file <- file.path("data", "EnergyRecords_Transport_extract_V5.xlsx")
 
