93370f431c642f5bac3d7bad3ff6aca32b9cfd98
Matthew Heun <matthew.heun@me.com>
2025-02-12 14:41:28 -0500

    New data from Ricardo.

--- pipeline.R
+++ pipeline.R
@@ -26,7 +26,7 @@ list(
     # Uncomment after debugging Rail data.
     # load_long_run_data(paths = c(ElectricityFilePath, TransportFilePath))
     load_long_run_data(paths = c(TransportFilePath)) |>
-      dplyr::filter(Year == 1814)
+      dplyr::filter(Year == 1815)
   ),
 
 
