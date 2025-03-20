32f28c17cafbe2b3e34312f866270dddee902f2b
Matthew Heun <matthew.heun@me.com>
2025-03-20 14:48:28 -0400

    Eliminated filter for years. Updated to latest transport file.

--- pipeline.R
+++ pipeline.R
@@ -25,8 +25,7 @@ list(
     LRData,
     # Uncomment after debugging Rail data.
     # load_long_run_data(paths = c(ElectricityFilePath, TransportFilePath))
-    load_long_run_data(paths = c(TransportFilePath)) |>
-      dplyr::filter(Year == 1815)
+    load_long_run_data(paths = c(TransportFilePath))
   ),
 
 
