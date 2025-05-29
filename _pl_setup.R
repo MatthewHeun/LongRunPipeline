f2ae16858ac626550343492d9f9df8e515a1a19a
Matthew Heun <matthew.heun@me.com>
2025-05-29 16:18:12 -0400

    Added debugging comment to top of file.

--- _pl_setup.R
+++ _pl_setup.R
@@ -1,4 +1,10 @@
 
+
+# For debugging: tar_make(callr_function = NULL, use_crew = FALSE, as_job = FALSE)
+# and
+# (1) insert browser() calls for functions in PFUPipeline2
+# (2) set breakpoints in functions from other packages.
+
 # Get file paths ---------------------------------------------------------------
 
 electricity_file <- file.path("data", "world_electricity_finalrevisited_v11.xlsx")
