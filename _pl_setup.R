ac927d565f7830b48c520faa9f8d1982415aec22
Matthew Heun <matthew.heun@me.com>
2025-07-11 15:50:32 -0400

    Change to new version of transport data.

--- _pl_setup.R
+++ _pl_setup.R
@@ -8,5 +8,5 @@
 # Get file paths ---------------------------------------------------------------
 
 electricity_file <- file.path("data", "world_electricity_finalrevisited_v14.xlsx")
-transport_file <- file.path("data", "EnergyRecords_Transport_extract_V12.xlsx")
+transport_file <- file.path("data", "EnergyRecords_Transport_extract_V13.xlsx")
 
